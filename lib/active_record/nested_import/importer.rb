67f42d6afc20bb2aa38007a7414821050a3e2f12
jiikko <n905i.1214@gmail.com>
2015-07-01 22:29:35 +0900

    oooooooooooooooooooops

--- lib/active_record/nested_import/importer.rb
+++ lib/active_record/nested_import/importer.rb
@@ -4,8 +4,6 @@ module ActiveRecord
       class BaseCollection
         attr_reader :association_name, :klass
         attr_accessor :prev_collection, :attrs, :ar_context
-        attr_accessor :through_attrs
-
 
         def initialize(klass, attrs, association_name, ar_context)
           @association_name = association_name
@@ -34,58 +32,65 @@ module ActiveRecord
           }
         end
 
-        def build_records
-          ar_context.public_send(association_name).build(attrs.uniq)
-        end
-      end
-
-      class FirstCollection < BaseCollection
         def setup
           if persisted_records.empty?
             @new_records = build_records
           else
             persisted_records.each { |x|
-              if (build_records.detect { |y| y.name == x.name }).nil?
+              if (build_records.detect { |y| equal_persistence_and_build?(x, y) }).nil?
                 @new_records.push(x)
               end
             }
           end
         end
 
-        def persisted_records
-          klass.where(collected_value_hash(attrs.uniq))
+        def build_records
+          ar_context.public_send(association_name).build(attrs.uniq)
         end
 
+      end
+
+      class FirstCollection < BaseCollection
         def create_next_collection(klass, association_name)
           NextCollection.new(klass, attrs, association_name, ar_context) do |x|
             x.prev_collection = self
           end
         end
+
+        private
+
+        def persisted_records
+          klass.where(collected_value_hash(attrs.uniq))
+        end
+
+        def equal_persistence_and_build?(x, y)
+          y.name == x.name
+          # TODO
+          # [{ name: :hoge, no: 1 }, { name: :hoge, no: 1 }] =>
+          # (x.name == y.name && x.no == y.no )
+        end
       end
 
       class NextCollection < BaseCollection
-        def setup
-          if persisted_records.empty?
-            @new_records = build_records
-          else
-            persisted_records.each { |x|
-              if (build_records.detect { |y| y.tag_id == x.tag_id } ).nil?
-                @new_records.push(x)
-              end
-            }
-          end
+        private
+
+        def equal_persistence_and_build?(x, y)
+          y.public_send("#{source_name}_id") == x.public_send("#{source_name}_id")
         end
 
         def attrs
-          source_key = ar_context.class.reflect_on_association(prev_collection.association_name).options[:source] || prev_collection.association_name.to_s.singularize
-          source_column_table = { id: "#{source_key}_id" }
+          source_column_table = { id: "#{source_name}_id" }
           prev_collection.klass.where(
             collected_value_hash(@attrs) # @attrs がmiso
           ).map { |x| { source_column_table[:id] => x.id } }
         end
 
         def persisted_records
-          ar_context.send(association_name).where(collected_value_hash(attrs))
+          ar_context.public_send(association_name).where(collected_value_hash(attrs))
+        end
+
+        def source_name
+          ar_context.class.reflect_on_association(prev_collection.association_name).options[:source] || prev_collection.association_name.to_s.singularize
         end
       end
 
